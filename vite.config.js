2d33cc6148d58c45ea50e1c3d2120e2460cf79fa
Prakan <prakansum@gosoft.co.th>
2024-11-16 16:07:30 +0700

    chang to jsx

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // ทำให้ Vite สามารถฟังที่ 0.0.0.0 ซึ่งรองรับการเข้าถึงจากภายนอก
+    port: 4173,        // กำหนดพอร์ตที่ต้องการใช้
+  },
 })
